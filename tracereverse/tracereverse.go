3e66eb2ab36a289921e321c1fe635b5dcc60bea3
tocatss <c_tian@dreamarts.co.jp>
2020-04-16 07:00:56 +0800

    Add incomplete permuteUnique

--- tracereverse/tracereverse.go
+++ tracereverse/tracereverse.go
@@ -1,5 +1,11 @@
+// Package: tracereverse is some practice from
 // https://liweiwei1419.gitee.io/leetcode-algo/leetcode-by-tag/backtracking/0046-permutations.html
 package tracereverse
+
+import (
+	"log"
+	"reflect"
+)
 
 func Permute(nums []int) [][]int {
 	stack := make(intStack, 0)
@@ -38,6 +44,57 @@ func Permute(nums []int) [][]int {
 	return res
 }
 
+// TODO: not complete.
+func PermuteUnique(nums []int) [][]int {
+	var (
+		ans      [][]int
+		dfs      func(visisted []bool, path *intStack)
+		visisted = make([]bool, len(nums))
+	)
+
+	dfs = func(visisted []bool, path *intStack) {
+		if path.len() == len(nums) {
+			c := path.copy()
+			ans = append(ans, *c)
+			return
+		}
+		copiedPath := path.copy()
+		for i, v := range nums {
+			if visisted[i] {
+				continue
+			}
+			path.push(v)
+			if path.equal(copiedPath) {
+				_ = path.pop()
+				continue
+			}
+			visisted[i] = true
+
+			dfs(visisted, path)
+
+			copiedPath = path.copy()
+			visisted[i] = false
+			_ = path.pop()
+		}
+	}
+
+	for i, v := range nums {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+		path := make(intStack, 0, len(nums))
+		visisted[i] = true
+		path.push(v)
+
+		dfs(visisted, &path)
+
+		visisted[i] = false
+		path.pop()
+	}
+	log.Print(ans)
+	return nil
+}
+
 type intStack []int
 
 func (s *intStack) push(item int) {
@@ -50,4 +107,15 @@ func (s *intStack) pop() int {
 	}
 	*s = ss[0 : len(ss)-1]
 	return ss[len(ss)-1]
+}
+func (s *intStack) equal(target *intStack) bool {
+	return reflect.DeepEqual(*s, *target)
+}
+func (s *intStack) len() int {
+	return len(*s)
+}
+func (s *intStack) copy() *intStack {
+	copied := make(intStack, len(*s))
+	copy(copied, *s)
+	return &copied
 }
--- tracereverse/tracereverse_test.go
+++ tracereverse/tracereverse_test.go
@@ -31,3 +31,30 @@ func Test_Permute(t *testing.T) {
 		})
 	}
 }
+
+// TODO: not complete.
+func Test_PermuteUnique(t *testing.T) {
+	tests := []struct {
+		name    string
+		payload []int
+		want    [][]int
+	}{
+		{
+			name:    "trace reverse",
+			payload: []int{1, 1, 2},
+			want: [][]int{
+				{1, 2, 3},
+				{1, 3, 2},
+				{2, 3, 1},
+				{2, 1, 3},
+				{3, 2, 1},
+				{3, 1, 2},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.ElementsMatch(t, tt.want, PermuteUnique(tt.payload))
+		})
+	}
+}
