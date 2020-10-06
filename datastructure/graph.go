913fcc1ce9c65d5b049060482da7d37176ca3eb3
tocatss <tocats@foxmail.com>
2020-10-06 10:27:23 +0800

    add shortest path

--- datastructure/graph.go
+++ datastructure/graph.go
@@ -37,3 +37,69 @@ func (g *graph) DFSTraverseBySlice() {
 		dfs(i)
 	}
 }
+
+// 最短路径
+//       B
+//   /       \
+// A - C - E - F - G
+//   \           /
+//         D
+// want : A,D,G
+func BFSShortestPath(graph map[string][]string, from, to string) []string {
+	if _, ok := graph[from]; !ok {
+		return nil
+	}
+
+	visited := make(map[string]bool)
+	ns := make([]*node, 0, 10)
+	ns = append(ns, &node{
+		parent: nil,
+		value:  from,
+	})
+
+	found := bfs(ns, graph, visited, to)
+	tail := make([]string, 0, 10)
+	for found != nil {
+		tail = append(tail, found.value)
+		found = found.parent
+	}
+	res := make([]string, 0, len(tail))
+	for i := len(tail) - 1; i >= 0; i-- {
+		res = append(res, tail[i])
+	}
+
+	return res
+}
+
+type node struct {
+	parent *node
+	value  string
+}
+
+func bfs(ns []*node, graph map[string][]string, visited map[string]bool, want string) *node {
+	for i := 0; i < len(ns); i++ {
+		n := ns[i]
+		next, ok := graph[n.value]
+		if !ok {
+			continue
+		}
+		for _, v := range next {
+			if _, ok := visited[v]; ok {
+				continue
+			}
+			visited[v] = true
+
+			if v == want {
+				return &node{
+					parent: n,
+					value:  v,
+				}
+			}
+			ns = append(ns, &node{
+				parent: n,
+				value:  v,
+			})
+		}
+	}
+	return nil
+}
--- datastructure/graph_test.go
+++ datastructure/graph_test.go
@@ -1,6 +1,10 @@
 package datastructure
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 // vo-v1
 //  |  |
@@ -21,4 +25,67 @@ var testGraph = &graph{
 
 func TestGraph_DFSTraverseBySlice(t *testing.T) {
 	testGraph.DFSTraverseBySlice()
+}
+func TestGraph_BFSShortestPath(t *testing.T) {
+	tests := []struct {
+		name  string
+		graph map[string][]string
+		from  string
+		to    string
+		want  []string
+	}{
+		//       B
+		//   /       \
+		// A - C - E - F - G
+		//   \            /
+		//          D
+		{
+			name: "from A to D",
+			graph: map[string][]string{
+				"A": {"B", "C", "D"},
+				"B": {"F"},
+				"C": {"E"},
+				"D": {"G"},
+				"E": {"F"},
+				"F": {"G"},
+			},
+			from: "A",
+			to:   "G",
+			want: []string{"A", "D", "G"},
+		},
+		{
+			name: "from C to G",
+			graph: map[string][]string{
+				"A": {"B", "C", "D"},
+				"B": {"F"},
+				"C": {"E"},
+				"D": {"G"},
+				"E": {"F"},
+				"F": {"G"},
+			},
+			from: "C",
+			to:   "G",
+			want: []string{"C", "E", "F", "G"},
+		},
+		{
+			name: "from A to ?",
+			graph: map[string][]string{
+				"A": {"B", "C", "D"},
+				"B": {"F"},
+				"C": {"E"},
+				"D": {"G"},
+				"E": {"F"},
+				"F": {"G"},
+			},
+			from: "A",
+			to:   "?",
+			want: []string{},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := BFSShortestPath(tt.graph, tt.from, tt.to)
+			assert.Equal(t, tt.want, got)
+		})
+	}
 }
